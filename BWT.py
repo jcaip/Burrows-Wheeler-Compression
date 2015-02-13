aca6650ab12256f612778e4bad08bd2f40237820
Jesse Cai <jcjessecai@gmail.com>
2015-02-12 23:02:05 -0800

    wrote BWT encode, made changes to CSA so it take linear space instead of quadratic

--- BWT.py
+++ BWT.py
@@ -1,7 +1,22 @@
 #TODO refactor code and split up into multiple classes: 
-# move to front; Huffman; CSA; and Burrows Wheeler
-from heapq import heappop
+# move to front; Huffman; CSA; and Burrows Wheeler from CSA import CSA
+from time import clock
+from CSA import CSA
+class BWT():
+    def encode(self,text):
+        c = CSA(text)
+        print(c)
+        t=''
+        for i in range(0,len(text)):
+            index = c.getIndex(i)
+            if index !=0:
+                t+=text[c.getIndex(i)-1]
+            else:
+                t =str(i)+t
+                t+=text[-1]
+        print(t)
+text = open('tests/6mb_test.txt','rb').read()
 
-def BWTransform():
-    print "hello world"
-    return 0
+t= clock()
+c = CSA(text)
+print(clock()-t)
--- CSA.py
+++ CSA.py
@@ -5,13 +5,24 @@ class CSA:
         self.index = self.makeCSA(text)
         
     def makeCSA(self,text):
-        arr, index = [], range(0, len(text))
-        for i in index:
-            arr.append(text[i:] + text[:i])
-        zipped = zip(index, arr)
-        zipped.sort(key=lambda t: t[1])
-        return list(zip(*zipped)[0])
-    
+        class csaIndex: #TODO switch python interperter to 2.7 and use cmp instead of thes bullshit
+
+            def __init__(self,i):
+                self.index = i
+
+            def __lt__(self,other):
+                i,j = self.index, other.index
+                while text[i] ==text[j]:
+                    i =(i+1)%len(text)
+                    j = (j+1)%len(text)
+                return (text[i] - text[j])<0
+            
+        
+        index = [csaIndex(i) for i in range(0, len(text))]
+        
+        index = sorted(index)
+        return [i.index for i in index]
+
     def getLen(self):
         return self.len
     def getIndex(self, i):

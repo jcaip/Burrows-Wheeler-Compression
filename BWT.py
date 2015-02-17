2219c0fd2c9e98fedb8c4ac89319e2f5c30b9020
Jesse Cai <jcjessecai@gmail.com>
2015-02-17 14:54:49 -0800

    wrote decode function for BWT

--- BWT.py
+++ BWT.py
@@ -19,20 +19,36 @@ class BWT():
                 t = i_bytes+t
                 t.append(text[-1])
         return t
+    
+    def decode(self,t):
+        #TODO write this function and make it work
+        index = int.from_bytes(bytes(t[:4]), byteorder = sys.byteorder,signed=False)
+        print(index)
+        text = bytearray()
+        next = []
+        text.lstrip()
+        first = sorted(t)
+        for c in first:
+            next.append(t.index(c))
+        for index in next:
+            text.append(t[index])
+        return text
+
+#testing code
 t = clock()
 
 b = BWT()
-m = moveToFront()
 h = Huffman()
 
 text = open('tests/6mb_test.txt','rb').read()
 
 text_BWT = b.encode(text)
-print(clock()-t)
+b.decode(text_BWT)
+"""print(clock()-t)
 text_MTF = m.moveToFrontEn(text_BWT)
 print(clock()-t)
 text_HUF = h.compress(text_MTF)
-print(clock()-t)
+print(clock()-t)"""
 
 f = open('output.enc','wb')
 text_HUF.tofile(f)
